f6613e679099c936d4d99de03bfde9888659bdfa
lchsk <mjlechowski@gmail.com>
2016-08-07 00:21:10 +0100

    Format mode line

--- lisp/mode-line.el
+++ lisp/mode-line.el
@@ -1,35 +1,108 @@
 (set-face-background 'mode-line "#EDC951")
 
-(setq-default mode-line-format
-	      '(
-		(:propertize ((:eval(format " %s " (window-numbering-get-number-string))
-				    ))
-face (:background "#eb6841" :foreground "white")
-			     )
-        ;; add a noticeable red block that says 'READ ONLY' when the file's, er, read only
-        (:eval
-         (when (eql buffer-read-only t)
-             (propertize " (R) " 'face
-                         '(:foreground "white" :background "#eb6841" :weight bold))))
-        (:eval
-         (propertize " %b " 'face
-                     (if (buffer-modified-p)
-                         '(:background "#CC333F" :foreground "white" :weight bold)
-                       '(:background "#00A0B0" :foreground "white" :weight bold))))
-	(:propertize (
-		      (:eval (format "%s" (if indent-tabs-mode " (T) " " (S) "))))
-		     face (:background "#6a4a3c" :foreground "white"))
-        (:propertize " %m " face (:background "#6A4A3C" :foreground "white"))
+(setq-defaultmode-line-format
+ '(
+   (:propertize (
+                 (:eval(
+                        format
+                        " %s "
+                        (window-numbering-get-number-string)
+                        )))
+                face (:background "#eb6841" :foreground "white"))
 
-        (:propertize (
-		      (:eval (
-                  concat (replace-regexp-in-string "^ Git[:-]" " " vc-mode) " " )))
-		     face (:weight normal :background "#EB6841" :foreground "white"))
-	(:propertize (
-		      (:eval (
-			      format " %s " (vc-state (buffer-file-name (current-buffer)))
-			      )))
-			      face (:weight: bold :background "#CC333F" :foreground "white"))
-        (:propertize " %l:%c " face (:background "#CC333F" :foreground "white" :weight light))))
+   (:eval
+    (
+     when (eql buffer-read-only t)
+      (propertize
+       " (R) "
+       'face '(
+               :foreground
+               "white"
+               :background
+               "#eb6841"
+               :weight
+               bold))))
+
+   (:eval
+    (propertize
+     " %b "
+     'face
+     (if (buffer-modified-p)
+         '(
+           :background
+           "#CC333F"
+           :foreground
+           "white"
+           :weight
+           bold)
+       '(
+         :background
+         "#00A0B0"
+         :foreground
+         "white"
+         :weight
+         bold))))
+
+   (:propertize (
+                 (:eval
+                  (format
+                   "%s"
+                   (if indent-tabs-mode " (T) " " (S) "))))
+                face (
+                      :background
+                      "#6a4a3c"
+                      :foreground
+                      "white"))
+
+   ;; Major mode
+   (:propertize
+    " %m "
+    face (
+          :background
+          "#6A4A3C"
+          :foreground
+          "white"))
+
+   (:propertize (
+                 (:eval (
+                         concat (
+                                 replace-regexp-in-string
+                                 "^ Git[:-]"
+                                 " "
+                                 vc-mode
+                                 )
+                                " ")))
+                face (
+                      :weight
+                      normal
+                      :background
+                      "#EB6841"
+                      :foreground
+                      "white"))
+
+   (:propertize (
+                 (:eval (
+                         format
+                         " %s "
+                         (vc-state
+                          (buffer-file-name
+                           (current-buffer))))))
+                face (
+                      :weight:
+                      bold
+                      :background
+                      "#CC333F"
+                      :foreground
+                      "white"))
+
+   (:propertize
+    " %l:%c "
+    face (
+          :background
+          "#CC333F"
+          :foreground
+          "white"
+          :weight
+          light))))
 
 (provide 'mode-line)
