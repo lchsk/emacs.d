30b1486340baf44e3d8362a1eecd666449b5c009
lchsk <mjlechowski@gmail.com>
2016-11-06 10:41:44 +0000

    Add create file function for dired

--- lisp/utility-funcs.el
+++ lisp/utility-funcs.el
@@ -1,3 +1,39 @@
+(eval-after-load 'dired
+  '(progn
+     (define-key dired-mode-map (kbd "c") 'my-dired-create-file)
+     (defun create-new-file (file-list)
+       (defun exsitp-untitled-x (file-list cnt)
+         (while (and (car file-list) (not (string= (car file-list) (""))))
+           (setq file-list (cdr file-list)))
+         (car file-list))
+
+       (defun exsitp-untitled (file-list)
+         (while (and (car file-list) (not (string= (car file-list) "")))
+           (setq file-list (cdr file-list)))
+         (car file-list))
+
+       (if (not (exsitp-untitled file-list))
+           ""
+         (let ((cnt 2))
+           (while (exsitp-untitled-x file-list cnt)
+             (setq cnt (1+ cnt)))
+           ("")
+           )
+         )
+       )
+     (defun my-dired-create-file (file)
+       (interactive
+        (list (read-file-name "Create file: " (concat (dired-current-directory) (create-new-file (directory-files (dired-current-directory))))))
+        )
+       (write-region "" nil (expand-file-name file) t)
+       (dired-add-file file)
+       (revert-buffer)
+       (dired-goto-file (expand-file-name file))
+       )
+     )
+  )
+
+
 (defun goto-first-reference ()
   (interactive)
   (eval
