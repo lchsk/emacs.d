8a30a7f29cba7a442bb708cbcef221a4c6d7c4fe
lchsk <mjlechowski@gmail.com>
2016-11-20 21:36:33 +0000

    Set zsh as default for multi-term

--- lisp/settings.el
+++ lisp/settings.el
@@ -28,7 +28,7 @@
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-hook 'js2-mode-hook (lambda () (setq js2-basic-offset 4)))
 
-(setq multi-term-program "/bin/bash")
+(setq multi-term-program "/bin/zsh")
 
 (setq backup-directory-alist `(("." . "~/.saves")))
 
