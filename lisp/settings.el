ffbd3381efa42d16723e05898ef1368b44a5cd48
lchsk <mjlechowski@gmail.com>
2017-05-08 10:30:04 +0100

    Fix tab-width for python-mode

--- lisp/settings.el
+++ lisp/settings.el
@@ -99,10 +99,12 @@
 
 (global-company-mode 1)
 
+(setq-default tab-width 4)
+
 (add-hook 'python-mode-hook
           (lambda ()
-            (setq-default tab-width 4)
             (setq c-basic-offset 4)
+            (setq tab-width 4)
             (rainbow-delimiters-mode 1)
             (setq python-indent-offset 4)
             (jedi:setup)
