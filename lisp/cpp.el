489ee0e9b3f8b81f73ceb9700a5ba8cae17842a0
lchsk <mjlechowski@gmail.com>
2018-12-27 04:02:12 +0000

    Set indentation and linux style for cpp mode

--- lisp/cpp.el
+++ lisp/cpp.el
@@ -1,7 +1,13 @@
+;; Indentation styles:
+;; https://www.emacswiki.org/emacs/IndentingC
 
 (defun my-c++-mode-hook ()
-  (global-set-key (kbd "C-x w") 'add-newlines-with-point-between))
+  (global-set-key (kbd "C-x W") 'add-newlines-with-point-between)
 
+  (c-set-style "Linux")
+  (setq-default indent-tabs-mode nil)
+  (setq-default tab-width 4)
+  (setq c-basic-offset tab-width))
 
 (add-hook 'c++-mode-hook 'my-c++-mode-hook)
 
