fd27955959fd021daa291bc39c0cf2d7a52b1d2c
lchsk <mjlechowski@gmail.com>
2019-11-29 16:32:23 +0000

    Set indent to 2

--- lisp/conf-js.el
+++ lisp/conf-js.el
@@ -3,12 +3,13 @@
 
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-hook 'js2-mode-hook (lambda ()
-                           (setq js2-basic-offset 4)
+                           (setq js2-basic-offset 2)
                            (when (> (buffer-size) (* 1024 20))
                              (linum-mode -1)
                              (fundamental-mode))))
+;; (setq tab-width-js 2)
 
-
-(defvaralias 'js-indent-level 'tab-width)
+;; (defvaralias 'js-indent-level 'tab-width)
+;; (defvaralias 'js-indent-level 'tab-width-js)
 
 (provide 'conf-js)
