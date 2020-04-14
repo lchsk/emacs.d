cc36be1a586cd57ca39bf70b4011189f172c93df
lchsk <mjlechowski@gmail.com>
2020-04-14 01:43:48 +0100

    Turn off js2-mode syntax checks

--- lisp/conf-js.el
+++ lisp/conf-js.el
@@ -4,6 +4,8 @@
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-hook 'js2-mode-hook (lambda ()
                            (setq js2-basic-offset 2)
+                           (setq js2-mode-show-parse-errors nil)
+                           (setq js2-mode-show-strict-warnings nil)
                            (when (> (buffer-size) (* 1024 20))
                              (linum-mode -1)
                              (fundamental-mode))))
