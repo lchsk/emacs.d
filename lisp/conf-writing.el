0edaeaf1db8781065b69a815f4ff3ea0cf1b65fc
lchsk <mjlechowski@gmail.com>
2020-05-05 18:34:16 +0100

    Add writing conf

--- lisp/conf-writing.el
+++ lisp/conf-writing.el
@@ -1,6 +1,44 @@
-(require-package 'darkroom)
 (require-package 'olivetti)
+(require-package 'flyspell-correct)
+(require-package 'flyspell-correct-helm)
+(require 'flyspell-correct-helm)
 
-(require 'darkroom)
+(require-package 'ac-ispell)
+
+(defun wc ()
+  (interactive)
+  (shell-command-to-string (concat "wc -w " buffer-file-name " | cut -f1 -d' ' | tr -d ' \t\n\r'"))
+  )
+
+(defun writer-mode ()
+  (interactive)
+  (olivetti-mode 1)
+  (set-default-font "DejaVu Sans-16")
+  (olivetti-set-width 100)
+
+  (define-key flyspell-mode-map (kbd "C-;") 'flyspell-correct-wrapper)
+
+  (defun update-mode-line ()
+    (setq-default mode-line-format (list "%b " (wc) " words"))
+    )
+
+  (defun my-after-save-actions ()
+    "Used in `after-save-hook'."
+    (update-mode-line)
+    )
+
+(add-hook 'after-save-hook 'my-after-save-actions)
+
+  (flyspell-mode)
+  (flyspell-buffer)
+  (message "Writer mode")
+  )
+
+
+(defun writer-mode-polish ()
+  (interactive)
+  (ispell-change-dictionary "polish")
+  (flyspell-buffer)
+  )
 
 (provide 'conf-writing)
